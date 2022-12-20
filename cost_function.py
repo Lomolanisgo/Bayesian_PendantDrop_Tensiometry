43ecaf5558d9a5867e95eaa72c3f4a2fd1f68fea
ZANG Yicheng <49782859+Lomolanisgo@users.noreply.github.com>
2022-12-20 15:22:48 +0100

    Update: Add crop_margin

--- cost_function.py
+++ cost_function.py
@@ -3,6 +3,7 @@ import glob
 import cv2
 from PIL import Image
 import numpy as np
+from skimage import io
 
 def __init__():
    return
@@ -62,3 +63,36 @@ def cost(img_ori,img_gen,size=100,output=0):
         return accurancy
     else:
         return accurancy,C
+
+def crop_margin(img,path='./cut_edge.png'):
+        img2=img.sum(axis=2)
+        (row,col)=img2.shape
+        row_top=0
+        raw_down=0
+        col_top=0
+        col_down=0
+        for r in range(0,row):
+                if img2.sum(axis=1)[r]<700*col:
+                        row_top=r
+                        break
+ 
+        for r in range(row-1,0,-1):
+                if img2.sum(axis=1)[r]<700*col:
+                        raw_down=r
+                        break
+ 
+        for c in range(0,col):
+                if img2.sum(axis=0)[c]<700*row:
+                        col_top=c
+                        break
+ 
+        for c in range(col-1,0,-1):
+                if img2.sum(axis=0)[c]<700*row:
+                        col_down=c
+                        break
+ 
+        new_img=img[row_top:raw_down+1,col_top:col_down+1,0:3]
+
+        io.imsave(path,new_img)
+
+        return new_img,path
