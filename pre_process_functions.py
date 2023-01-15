ee1f51ec9a02a5ef27c086a25c9716356f872cf5
Lomolanisgo <zyc9@outlook.com>
2023-01-15 03:44:29 +0100

    Update pre_process_functions.py

--- pre_process_functions.py
+++ pre_process_functions.py
@@ -99,3 +99,36 @@ def crop_margin(img,path='./images_notupload/cut_edge.png'):
         io.imsave(path,new_img)
 
         return new_img,path
+
+def crop_margin_array(img,path='./images_notupload/cut_edge.png'):#(Not finish)
+        img2=img.sum(axis=2)
+        (row,col)=img2.shape
+        row_top=0
+        raw_down=0
+        col_top=0
+        col_down=0
+        for r in range(0,row):
+                if img2.sum(axis=1)[r]<700*col:
+                        row_top=r
+                        break
+ 
+        for r in range(row-1,0,-1):
+                if img2.sum(axis=1)[r]<700*col:
+                        raw_down=r
+                        break
+ 
+        for c in range(0,col):
+                if img2.sum(axis=0)[c]<700*row:
+                        col_top=c
+                        break
+ 
+        for c in range(col-1,0,-1):
+                if img2.sum(axis=0)[c]<700*row:
+                        col_down=c
+                        break
+ 
+        new_img=img[row_top:raw_down+1,col_top:col_down+1,0:3]
+
+        io.imsave(path,new_img)
+
+        return new_img,path
