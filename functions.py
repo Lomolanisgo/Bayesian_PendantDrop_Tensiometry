b04160028ffce51f21848bb0079fb16fa30113ca
Lomolanisgo <zyc9@outlook.com>
2023-01-31 11:24:54 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -81,6 +81,8 @@ def genDropletNoMargin(sigma,volume0,rneedle=0.5):
     end=0
     left_edge=np.zeros(h_syn)
     right_edge=np.zeros(h_syn)
+    p_needle=1
+    d_needle=50
     for i in range(h_syn):
         left=0
         right=0 
@@ -116,6 +118,7 @@ def genDropletNoMargin(sigma,volume0,rneedle=0.5):
     syn50[syn50>=125]=255
 
     # cut bottom margin
+    cut_b=h_syn
     h_syn,w_syn=syn50.shape
     for i in range (h_syn):
         if sum(syn50[i,:])==255*w_syn and sum(syn50[i-1])<255*w_syn:
@@ -162,7 +165,7 @@ def lostFunction(exp_arr,syn_arr,output=0):
     else:
         return lost,C
 
-def lhood(sv,path_exp,output=0):    
+def likelihood(sv,path_exp,output=0):    
     sigma=sv[0]; v0=sv[1]
 
     exp_arr=removeStandNeedleCentering(path_exp)
@@ -173,3 +176,52 @@ def lhood(sv,path_exp,output=0):
         return lost
     else:
         return lost,C
+
+def genDropletWithNeedle(sigma,volume0,rneedle=0.5):
+    r,z=genSingleDrop(sigma=sigma,rneedle=rneedle,volume0=volume0,output=1)    
+    plt.plot(r,z,'black')
+    plt.plot(-r,z,'black')
+    plt.axis('equal')
+    plt.axis('off')
+    figure = plt.gcf().canvas
+    ag = figure.switch_backends(FigureCanvasAgg)
+    ag.draw()
+    plt.close()
+    A = np.asarray(ag.buffer_rgba())
+    syn_arr = np.rint(A[...,:3] @ [0.2126, 0.7152, 0.0722]).astype(np.uint8)
+
+    h_syn,w_syn=syn_arr.shape
+
+    # fill the droplet to black
+    end=0
+    left_edge=np.zeros(h_syn)
+    right_edge=np.zeros(h_syn)
+    p_needle=1
+    d_needle=50
+    for i in range(h_syn):
+        left=0
+        right=0 
+        for j in range (w_syn):
+            if syn_arr[i,j-1]>=125 and syn_arr[i,j]<125 and j < w_syn/2:
+                left=j
+                end=end+1
+        for k in range (w_syn-1,0,-1):
+            if syn_arr[i,k]>=125 and syn_arr[i,k-1]<125 and k > w_syn/2:
+                right=k
+        for l in range (w_syn):
+            if l>left and l<right:
+                syn_arr[i,l]=0
+        left_edge[i]=left
+        right_edge[i]=right
+        if left!=0 and end==1:
+            p_needle=i
+
+    for i in range(h_syn):
+        if i<=p_needle:
+            syn_arr[i,int(left_edge[p_needle]):int(right_edge[p_needle])]=0    
+
+    img_arr=Image.fromarray(syn_arr)
+    img_arr.save('./s%.2f_v%.2f_n%.2f.jpg'%(sigma,volume0,rneedle))
+    
+
+    return img_arr
